d14bebe509d94fef5ddc1161c1d0bd5bce1088db
badri2104 <93633615+badri2104@users.noreply.github.com>
2022-02-19 10:16:55 +0530

    Day - 2 Assignment

---
+++ Day - 4/Q1.py
@@ -0,0 +1,10 @@
+# Q1. Python program to print Highest Common Factor (HCF) of two numbers
+x = 50
+y = 100
+if x > y:
+  x, y = y, x
+for i in range(1,x+1):
+  if x%i == 0 and y%i == 0:
+    hcf = i
+
+print("HCF of", x, "and", y, "is:", hcf)
